8f326c296f41b8e369e881fb7bda0b32fdd9f324
Arte Ivanov <artiv85@gmail.com>
2024-03-06 22:09:30 +0300

    Добавил вторую функцию для создания нового массива по условию

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string[] srt = ["Hello", "2", "world", ":-)"];
+﻿using System.Runtime.ExceptionServices;
+
+string[] str = ["Hello", "2", "world", ":-)"];
 
 
 int CountElement(string[] array)
@@ -15,4 +17,20 @@ int CountElement(string[] array)
     return count;
 }
 
-Console.Write($"{CountElement(srt)}");
+string[] NewArray(string[] array, int count)
+{
+    string[] result = new string[2];
+    int j = 0;
+    for (int i =0; i<array.Length; i++)
+    {
+        if (array[i].Length<=3)
+            {
+                result[j]=array[i];
+                j++;
+            }
+    }
+    return result;
+}
+
+Console.Write($"Изначальный массив: [{string.Join(";",str)}]");
+Console.Write($"  [{string.Join(";",NewArray(str, CountElement(str))    )}]");
